b3f13cca4a5030edc039beceed497d9fd542f7a0
Muramatsu naoya <sh.mn.nat@gmail.com>
2015-07-22 14:03:23 +0900

    rotation red sphere

--- test_openglut.py
+++ test_openglut.py
@@ -5,47 +5,79 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
 
-def main():
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH)
-    glutInitWindowSize(300, 300)  # ウィンドウサイズ
-    glutInitWindowPosition(100, 100)  # ウィンドウ位置
-    glutCreateWindow("test")  # ウィンドウを表示
-    glutDisplayFunc(display)  # 描画コールバック関数を登録
-    glutReshapeFunc(reshape)  # リサイズコールバック関数の登録
-    init(300, 300)
-    glutMainLoop()
 
+# === Global Value ===
+WIDTH = 300
+HEIGHT = 300
+
+angle = 0.0
+
+
+# === INIT ===
 def init(width, height):
-    """初期化"""
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    glEnable(GL_DEPTH_TEST)  # 隠面消去を有効に
+	glClearColor(0.0, 0.0, 0.0, 1.0)
+	glEnable(GL_DEPTH_TEST)  # 隠面消去を有効に
 
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)  # 投影変換
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
 
+	gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)  # 投影変換
+
+
+
+# === View Screen ===
 def display():
-    """描画処理"""
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    # 視野変換：カメラの位置と方向のセット
-    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-    
-    # ティーポットを描画
-    glColor3f(1.0, 0.0, 0.0)
-    glutSolidTeapot(1.0)
-#    glutWireTeapot(1.0)
-    glFlush()  # OpenGLコマンドの強制実行
+	glMatrixMode(GL_MODELVIEW)
+	glLoadIdentity()
+	# 視野変換：カメラの位置と方向のセット
+	gluLookAt(5.0,4.0,1.0, 0.0,0.0,0.0, 0.0,0.0,1.0)	# camera coordinates, camera angle, camera top
+	
+	# draw objects
+	glRotatef(angle, 0.0, 0.0, 1.0)	# rotation
+	glColor3f(1.0, 0.0, 0.0)	# (rad, green, blue)   0.0~1.0
+	glutWireSphere(1.0, 30, 30)	# (radius, partitions of mdridian, parallel)
 
+	glFlush()  # OpenGLコマンドの強制実行
+
+
+
+# === Resize ===
 def reshape(width, height):
-    """画面サイズの変更時に呼び出されるコールバック関数"""
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)
+	"""画面サイズの変更時に呼び出されるコールバック関数"""
+	glViewport(0, 0, width, height)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)
+
+
+# === IDLE ===
+def idle():
+	"""アイドル時に呼ばれるコールバック関数"""
+	global angle
+	angle += 0.05
+	glutPostRedisplay()	# redisplay
+
+
+
+# === MAIN ===
+def main():
+	# initial processing
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH)
+	glutInitWindowSize(WIDTH, HEIGHT)	# ウィンドウサイズ
+	glutInitWindowPosition(100, 100)	# ウィンドウ位置
+	glutCreateWindow(u"Test")	# ウィンドウを表示
+	glutDisplayFunc(display)	# 描画コールバック関数を登録
+	glutReshapeFunc(reshape)	# リサイズコールバック関数の登録
+	glutIdleFunc(idle)			# アイドルコールバック関数を登録
+
+	init(WIDTH, HEIGHT)
+
+	# loop
+	glutMainLoop()
+
 
 if __name__ == "__main__":
-    main()
+	main()
